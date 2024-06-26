7eb41498f7e1f0dfd07ea421372b204f69e1e03e
JulieEdwardsPaleo <34520399+JulieEdwardsPaleo@users.noreply.github.com>
2024-06-25 19:30:10 -0700

    Add files via upload

--- 5_BuildChronology_1.0.R
+++ 5_BuildChronology_1.0.R
@@ -3,7 +3,6 @@ library(dplR) #import for chronology building
 rm(list=ls())
 
 ####LCP version
-####### test chron.stabilized(x, winLength, biweight = TRUE, running.rbar = FALSE)
 
 #Calling arguments if R script is called from python
 args = commandArgs(trailingOnly=TRUE)
@@ -18,26 +17,31 @@ setwd(Workdir3)
 Files=startsWith(list.files(Workdir3),'gap') #find files that have been  gap-filled
 filelist=list.files(Workdir3)[Files] #make list of files to iterate through
 
-
+exclude=read.csv("_YearsToExclude.txt",sep="\t")
 
 for(i in 1:length(filelist)){
 
   series=read.rwl(filelist[i])
-  #write.csv(series,paste(Workdir4,"ARSTAN_",filelist[i],sep=''))#loading in gap_filled summary files 
   write.csv(summary(series),paste(Workdir4,"RWL_summary_",filelist[i],sep='')) # write summary stats of series
   
-  detrended=detrend(series,make.plot = FALSE,
-                    method = c("Spline"), nyrs=30,f = 0.5, pos.slope = FALSE,difference=FALSE) #Detrend with a 2/3 segment length spline (default length)
-
-  
-
-  chrono=chron(detrended,biweight = TRUE) #create chronology
   
   trunc30=detrend(series[rownames(series)>1916,],
-                method = c("Spline"), nyrs = 30,f = 0.5, pos.slope = FALSE,difference=FALSE) #Detrend with a 2/3 segment length spline (default length)
-  write.csv(rwi.stats(trunc30,period = 'common'),paste(Workdir4,"RWI_stats",filelist[i],sep=''))
+                method = c("Spline"), nyrs = 30,f = 0.5, pos.slope = FALSE,difference=FALSE) #Detrend with a30yr spline 
+  Badremoved=trunc30
+   for (j in 1:nrow(exclude)) {
+    woodid <- exclude$WOODID[j]
+    year <- as.character(exclude$YEAR[j])
+    
+    # Check if the current 'WOODID' and 'year' exist in 'series' DataFrame
+    if (woodid %in% colnames(trunc30) && year %in% rownames(trunc30)) {
+      # Set the matching cell value to NaN
+      Badremoved[year, woodid] <- NaN
+      }
+    }
+
+  write.csv(rwi.stats(Badremoved,period = 'common'),paste(Workdir4,"RWI_stats",filelist[i],sep=''))
   
-  truncChrono=chron(trunc30,biweight = TRUE)
+  truncChrono=chron(Badremoved,biweight = TRUE)
   write.csv(truncChrono,paste(Workdir4,"Spline30yr_",filelist[i],sep=''))
   
 
